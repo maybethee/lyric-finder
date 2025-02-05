64d1f07325a5e38a51a7d5ceae8cff69942cf425
retra <jozalamun@gmail.com>
2025-02-04 20:42:36 -0500

    Clean lyrics and save to txt file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import re
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from lyricsgenius import Genius
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +14,7 @@ client_secret = os.getenv("SPOTIPY_CLIENT_SECRET")
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+genius = Genius(os.getenv("GENIUS_CLIENT_ACCESS_TOKEN"))
 
 def get_playlist_tracks(playlist_id):
   results = sp.playlist_tracks(playlist_id)
@@ -40,5 +44,38 @@ def list_tracks(tracks):
 playlist_id = "7ieg8bD3edzzcbwNDu5BEv"
 tracklist = list_tracks(get_playlist_tracks(playlist_id))
 
+
+
+def clean_lyrics(lyrics):
+  cleaned = re.sub(r'^.*?Lyrics', '', lyrics, flags=re.DOTALL).strip()
+  
+  cleaned = re.sub(r'See .*? LiveGet tickets as low as .*?You might also like', '', cleaned, flags=re.DOTALL).strip()
+
+  cleaned = re.sub(r'\d+Embed.*$', '', cleaned, flags=re.DOTALL).strip()
+
+  return cleaned
+
+# for i in range(20, 35):
+#   song = genius.search_song(tracklist[i]['name'], tracklist[i]['artist'])
+
+#   if song:
+#       cleaned_lyrics = clean_lyrics(song.lyrics)
+#       tracklist[i]['lyrics'] = cleaned_lyrics
+#   else:
+#       tracklist[i]['lyrics'] = None
+
+  # print(f"Cleaned lyrics for {tracklist[i]['name']}:")
+  # print(tracklist[i]['lyrics'])
+
 for track_dict in tracklist:
-  print(f"name: {track_dict['name']}\nartist: {track_dict['artist']}")
+
+  song = genius.search_song(track_dict['name'], track_dict['artist'])
+
+  if song:
+    cleaned_lyrics = clean_lyrics(song.lyrics)
+    track_dict['lyrics'] = cleaned_lyrics
+  else:
+    track_dict['lyrics'] = None
+
+with open('playlist-tracks', 'w') as fout:
+  json.dump(tracklist, fout)
